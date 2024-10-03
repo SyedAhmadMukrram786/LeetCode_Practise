92f50a936bfe5265b01e8bcaf38085c369c3ed31
SyedAhmadMukrram786 <124423665+SyedAhmadMukrram786@users.noreply.github.com>
2024-10-03 23:01:04 +0500

    Time: 206 ms (43.05%) | Memory: 120.2 MB (33.57%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_map<int,int> fr;
+        for(int n:candyType)
+        {
+            fr[n];
+        }
+        if(candyType.size()/2<=fr.size())
+        {
+            return  candyType.size()/2;
+        }
+        else
+        {
+            return fr.size();
+        }
+    }
+};
