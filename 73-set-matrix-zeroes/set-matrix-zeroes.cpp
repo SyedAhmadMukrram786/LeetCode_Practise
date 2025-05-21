6f27f1b9bb797424ef986042eaa1bdf6c84f12fb
SyedAhmadMukrram786 <124423665+SyedAhmadMukrram786@users.noreply.github.com>
2025-05-22 00:42:26 +0500

    Time: 0 ms (100.00%) | Memory: 18.6 MB (52.18%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> mat=matrix;
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j=0;j<matrix[i].size();j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    for(int k=j+1;k<matrix[i].size();k++)
-                    {
-                        mat[i][k]=0;
-                    }
-                    for(int k=j-1;k>=0;k--)
-                    {
-                        mat[i][k]=0;
-                    }
-                    for(int k=i-1;k>=0;k--)
-                    {
-                        mat[k][j]=0;
-                    }
-                    for(int k=i+1;k<matrix.size();k++)
-                    {
-                        mat[k][j]=0;
-                    }
+        vector<vector<int>> zero;
+
+        for(int i=0;i<matrix.size();i++) {
+            for(int j=0;j<matrix[i].size();j++) {
+                if(matrix[i][j]==0) {
+                    vector<int> temp;
+                    temp.push_back(i);
+                    temp.push_back(j);
+                    zero.push_back(temp);
                 }
             }
         }
-        matrix=mat;
+
+        for(int i=0;i<zero.size();i++) {
+            for(int j=0;j<matrix.size();j++) {
+                matrix[j][zero[i][1]]=0;
+            }
+            for(int j=0;j<matrix[0].size();j++) {
+                matrix[zero[i][0]][j]=0;
+            }
+        }
     }
 };
